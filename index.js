54a9314dd03c55b20b3b6c08fcf7a84d180a190c
illya <illyaklus2@gmail.com>
2026-03-02 13:41:57 +0200

    path fixes

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ const loadHTML = async (id, file) => {
 }
 
 async function initialize(){
-    await loadHTML('header', '../src/features/header/header.html');
+    await loadHTML('header', './src/features/header/header.html');
     createMenu();
 
-    await loadHTML('main', '../src/features/products/products.html')
+    await loadHTML('main', './src/features/products/products.html')
 
-    await loadHTML('footer', '../src/features/footer/footer.html');
+    await loadHTML('footer', './src/features/footer/footer.html');
 }
 
 
@@ -44,45 +44,45 @@ async function handleRoute() {
 
     switch (route) {
         case '#cart':
-            await loadHTML('main', '../src/features/cart/cart.html');
+            await loadHTML('main', './src/features/cart/cart.html');
             renderCart();
             updateClearCartState();
             break;
   
         case '#login':
-            await loadHTML('main', '../src/features/auth/login.html');
+            await loadHTML('main', './src/features/auth/login.html');
             initAuth();
             break;
   
         case '#register':
-            await loadHTML('main', '../src/features/auth/register.html');
+            await loadHTML('main', './src/features/auth/register.html');
             initAuth();
             break;
         
         case '#discounts':
-            await loadHTML('main','../src/features/discount/discounts.html');
+            await loadHTML('main','./src/features/discount/discounts.html');
             break
   
         case '#profile':
-            await loadHTML('main','../src/features/profile/profile.html');
+            await loadHTML('main','./src/features/profile/profile.html');
             break
   
         case '#about_us':
-            await loadHTML('main','../src/features/about_us/about_us.html');
+            await loadHTML('main','./src/features/about_us/about_us.html');
             break
   
         case '#profile-history':
-            await loadHTML('profile_add_info','../src/features/profile/profile_history/profile_history.html');
+            await loadHTML('profile_add_info','./src/features/profile/profile_history/profile_history.html');
             break
       
         case '#profile-selected':
-            await loadHTML('profile_add_info','../src/features/cart/cart.html');
+            await loadHTML('profile_add_info','./src/features/cart/cart.html');
             renderCart();
             updateClearCartState();
             break
   
         default:
-            const mainEl = await loadHTML('main','../src/features/products/products.html');
+            const mainEl = await loadHTML('main','./src/features/products/products.html');
   
             let div = document.getElementById('products_container');
             downloadProducts(div);
